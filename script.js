692c408076156108de8954189c08bfbd1e9bb3f8
Edilca <edilca@gmail.com>
2022-05-01 15:51:29 -0300

    pequenas alterações

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function carregar() {
-    var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('imagem');
-    var data = new Date();
-    var hora = data.getHours();
-    var min = data.getMinutes();
-    msg.innerHTML = `Agora são ${hora} hora e ${min} minutos.`;
-    if (hora >= 0 && hora < 12) {
-        img.src = '/img/dia.jpg';
-        document.body.style.background = '#e2cd9f';
-    } else if (hora >= 12 && hora <= 18) {
-        img.src = '/img/tarde.jpg';
-        document.body.style.background = '#b9846f';
-    } else {
-        img.src = '/img/noite.jpg';
-        document.body.style.background = '#515154';
-    }
+  var msg = window.document.getElementById('msg');
+  var img = window.document.getElementById('imagem');
+  var data = new Date();
+  var hora = data.getHours();
+  var min = data.getMinutes();
+  //var hora = ;
+  msg.innerHTML = `Agora são ${hora} hora e ${min} minutos.`;
+  if (hora >= 0 && hora < 12) {
+    img.src = './img/dia.jpg';
+    document.body.style.background = '#e2cd9f';
+  } else if (hora >= 12 && hora <= 18) {
+    img.src = './img/tarde.jpg';
+    document.body.style.background = '#b9846f';
+  } else {
+    img.src = './img/noite.jpg';
+    document.body.style.background = '#515154';
+  }
 }
